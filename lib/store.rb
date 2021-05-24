0deca145c6fda8e0fd2cf5b5ba0b5ad99ef6ed59
Zackory Kelly <zackkelly@live.ca>
2021-05-24 19:46:27 +0000

    Add stretch validation

--- lib/store.rb
+++ lib/store.rb
@@ -2,5 +2,14 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+  validate :mens_or_womens
+
+  private
+
+  def mens_or_womens
+    if mens_apparel.blank? && womens_apparel.blank?
+      errors.add(:base, "Store must sell atleast one of: Men's Apparel, Women's Apparel")
+    end
+  end
 
 end
